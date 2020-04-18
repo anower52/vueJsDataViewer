24aec266c23491ce9ddfe3c46f5daea3f596d373
anower00 <anower.hasan@gmail.com>
2020-04-18 06:52:00 +0600

    Data show from api in table with progressbar

--- resources/js/components/CustomerComponent.vue
+++ resources/js/components/CustomerComponent.vue
@@ -4,35 +4,37 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Customers</div>
-
                     <div class="card-body">
                         <div class="table-responsive-sm">
                             <table class="table table-hover table-bordered table-striped">
                                 <thead>
                                 <tr>
                                     <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Phone</th>
+                                    <th scope="col">Total</th>
+                                    <th scope="col" class="text-center">Action</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Larry the Bird</td>
-                                    <td>@twitter</td>
+                                <tr v-for="(customer, index) in customers" :key="customer.id">
+                                    <th scope="row">{{ index + 1}}</th>
+                                    <td>{{ customer.name}}</td>
+                                    <td>{{ customer.email}}</td>
+                                    <td>{{ customer.phone}}</td>
+                                    <td>{{ customer.total}}</td>
+                                    <td class="text-center">
+                                        <button type="button" class="btn btn-info">
+                                            <i class="fas fa-eye"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-primary">
+                                            <i class="fas fa-edit"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-danger">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </td>
                                 </tr>
                                 </tbody>
                             </table>
@@ -41,13 +43,34 @@
                 </div>
             </div>
         </div>
+        <vue-progress-bar></vue-progress-bar>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                customers: [],
+            }
+        },
         mounted() {
             console.log('Component mounted.')
+            this.getData();
+        },
+        methods: {
+            getData() {
+                this.$Progress.start()
+                axios.get('/vuedataviewer/public/api/customers')
+                    .then(response => {
+                        this.customers = response.data.data
+                        this.$Progress.finish()
+                    })
+                    .catch(e => {
+                        console.log(e)
+                        this.$Progress.fail()
+                    })
+            }
         }
     }
 </script>
